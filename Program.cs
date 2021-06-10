1c93567a83b15df220dba0b7c922d3b128b57b70
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-10 13:17:49 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Operators_Exercise
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+         
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine("{0}/{1} is {2} remainder {3}", a, b, quotient, remainder);
+
+            Console.WriteLine("What is the radius of your circle?");
+            var r = double.Parse(Console.ReadLine());
+
+            double answer = AreaOfCircle(r);
+
+
+
+
+            Console.WriteLine("The area of a circle with radius {0} is {1} ", r, answer);
+        }
+
+        private static double AreaOfCircle(double radius) => Math.PI * radius * radius; 
+
+      
+    }
+
+}
